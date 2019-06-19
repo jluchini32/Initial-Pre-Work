f5e0f2625266df87d97d8bed351014cc7c521d5d
Joe Luchini <joeluchini@ymail.com>
2019-06-19 17:28:46 -0400

    almost halfway

---
+++ 5-Introduction-to-JavaScript/45-Shopping_List.js
@@ -0,0 +1,2 @@
+var myList = [["Pizza Dough", 15], ["Banana",12], ["Peanut Butter", 2], ["Pizza", 100], ["Eggs", 1500]];
+
---
+++ 5-Introduction-to-JavaScript/51-Return_a_Value_from_a_Function_with_Return.js
@@ -0,0 +1,17 @@
+// Example
+function minusSeven(num) {
+    return num - 7;
+  }
+  
+  // Only change code below this line
+  function plusThree(num){
+    return num + 3;
+  }
+  
+  function timesFive(num){
+    return num * 5;
+  }
+  
+  console.log(minusSeven(10)); // returns 3
+  console.log(plusThree(111)); // returns 114
+  console.log(timesFive(5)); // returns 25
