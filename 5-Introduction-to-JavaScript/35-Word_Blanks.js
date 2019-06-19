baf396791f1f3a81d86b879aca999cbec13d6e79
Joe Luchini <joeluchini@ymail.com>
2019-06-19 14:53:22 -0400

    arrays and other fun things

---
+++ 5-Introduction-to-JavaScript/35-Word_Blanks.js
@@ -0,0 +1,13 @@
+//This one I struggled with, I used the "Get help" button on freecodecamp to see exactly how I was supposed to have this answer.
+
+
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+    // Your code below this line
+  
+      var result = "My " + myAdjective + " " + myNoun + " "+ myVerb + " very " + myAdverb + ".";
+    // Your code above this line
+    return result;
+  }
+  
+  // Change the words here to test your function
+  wordBlanks("dog", "big", "ran", "quickly");
