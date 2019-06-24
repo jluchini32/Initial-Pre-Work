4671dc6a070521fb5b3201cb52f6f77f74a6433f
Joe Luchini <joeluchini@ymail.com>
2019-06-24 16:20:47 -0600

    added to es6 summary

--- 6-Introduction-to-the-ES6-Challenges/1-Explore-Differences-Between-the-var-and-let-Keywords.js
+++ 6-Introduction-to-the-ES6-Challenges/1-Explore-Differences-Between-the-var-and-let-Keywords.js
@@ -8,7 +8,19 @@
 //Im also learning a ton about grabbing and reassingning variables in ES6 with the destructuring of 
 //variables in objects, which took me awhile to grasp. But once I started practing and gettting
 //the hang of the syntax, it made alot more sense. It really started to click when I got to assign
-//values from arrays
+//values from arrays. I'm also happy that this section touched a bit on Classes, which are basically
+//like a blueprint for creating objects. Although, I dont know if I will ever understand fully what
+//"this" means, I am getting better at using this to mutate or avoid mutating (freeze) elements and variables. I think 
+//that I am a better programmer because of ES6, object orieneted programming with classes
+// makes a little more sense to me. After I am done with this pre-work, I intend to go back 
+//through both the javascript section and ES6 section to better understand all of the syntax and better
+//prepare me for any future whiteboarding challenges, writing Vanilla Javascript is super important
+//and isnt going anywhere, so the better I can understand those concepts, the better off I will
+//be in the future for understanding complex concepts and algorithms. 
+
+
+
+
 
 //This is the first type of comment
 
